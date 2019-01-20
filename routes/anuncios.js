60dd3ea6c94c879008b4b78bb9af28835b41d219
ALVARO MILLAN <amillan@protonmail.com>
2019-01-20 21:51:13 +0100

    Filtros y paginación

--- routes/anuncios.js
+++ routes/anuncios.js
@@ -5,9 +5,72 @@ var Anuncio = mongoose.model('anuncio');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  Anuncio.find({}, (err, anuncios) => {
-        res.render('anuncio', { anuncios: anuncios });
-        // return res.status(200).json(anuncios);
+  let query = Anuncio.find({});
+  var pageOptions = {
+    page: req.query.page || 0,
+    limit: parseInt(req.query.limit) || 6
+  }
+
+  if (req.query.tag) {
+    query.where('tags').in(req.query.tag);
+  }
+
+  if (req.query.venta) {
+    query.where('venta').equals(req.query.venta);
+  }
+
+  if (req.query.nombre) {
+    const expression = new RegExp('^'+ req.query.nombre, "i");
+    query.where('nombre').regex(expression);
+  }
+
+  if (req.query.precio) {
+    const res = req.query.precio.split('-');
+    if (res.length == 1) {
+      query.where('precio').equals(res[0]);
+    } else {
+      if (res[0]) {
+        query.where('precio').gte(res[0]);
+      }
+  
+      if (res[1]) {
+        query.where('precio').lte(res[1]);
+      }
+  
+      if (res[0] && res[1]) {
+        query.where('precio').gte(res[0]).lte(res[1]);
+      }
+    }
+  }
+
+  if (req.query.sort) {
+    query.sort(req.query.sort);
+  }
+  
+  query.skip(pageOptions.page*pageOptions.limit)
+  query.limit(pageOptions.limit)
+
+  query.exec((err, anuncios) => {
+    console.log(err);
+    res.render('anuncio', { anuncios: anuncios });
+  });
+  
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req);
+  Anuncio.insertMany(req.body, (err, anuncio) => {
+    res.status(201).json({
+        ok: true,
+        anuncio: anuncio
+    })
+  });
+
+});
+
+router.get('/tags', function(req, res, next) {
+  Anuncio.find({}).distinct('tags', function(err, tags) {
+      res.status(200).send(JSON.stringify(tags));
   });
 });
 
